ee38a83566a2cf9b490f422788f53068280e17b1
Lufsx <Lufs00@outlook.com>
2022-03-10 23:47:58 +0800

    fix: error

--- components/previews/VideoPreview.tsx
+++ components/previews/VideoPreview.tsx
@@ -132,14 +132,14 @@ const VideoPreview: FC<{ file: OdFileObject }> = ({ file }) => {
             btnText={t('Download')}
             btnIcon="file-download"
           />
-          <DownloadButton
+          {/* <DownloadButton
             onClickCallback={() =>
-              window.open(`https://proxy.lufs.workers.dev?url=${encodeURIComponent(...)}`)
+              window.open(`/api/proxy?url=${encodeURIComponent(...)}`)
             }
             btnColor="teal"
             btnText={t('Proxy download')}
             btnIcon="download"
-          />
+          /> */}
           <DownloadButton
             onClickCallback={() => {
               clipboard.copy(`${getBaseUrl()}/api/raw/?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`)
