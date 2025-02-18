6319743acd831eec336173474b14c163943388d7
LufsX <lufsx@outlook.com>
2025-02-18 11:35:39 +0800

    fix: plyr error and remove video player

--- src/components/previews/VideoPreview.tsx
+++ src/components/previews/VideoPreview.tsx
@@ -1,78 +1,22 @@
 import type { OdFileObject } from '../../types'
 
-import { FC, useEffect, useState } from 'react'
+import { FC, useState } from 'react'
 import { useRouter } from 'next/router'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useTranslation } from 'next-i18next'
 
-import axios from 'axios'
 import toast from 'react-hot-toast'
-import Plyr from 'plyr-react'
-import { useAsync } from 'react-async-hook'
 import { useClipboard } from 'use-clipboard-copy'
 
 import { getBaseUrl } from '../../utils/getBaseUrl'
-import { getExtension } from '../../utils/getFileIcon'
 import { getStoredToken } from '../../utils/protectedRouteHandler'
+import { getFileIcon } from '../../utils/getFileIcon'
+import { formatModifiedDateTime, humanFileSize } from '../../utils/fileDetails'
 
 import { DownloadButton } from '../DownloadBtnGtoup'
 import { DownloadBtnContainer, PreviewContainer } from './Containers'
-import FourOhFour from '../FourOhFour'
-import Loading from '../Loading'
 import CustomEmbedLinkMenu from '../CustomEmbedLinkMenu'
-
-import 'plyr-react/plyr.css'
-
-const VideoPlayer: FC<{
-  videoName: string
-  videoUrl: string
-  width?: number
-  height?: number
-  thumbnail: string
-  subtitle: string
-  isFlv: boolean
-  mpegts: any
-}> = ({ videoName, videoUrl, width, height, thumbnail, subtitle, isFlv, mpegts }) => {
-  useEffect(() => {
-    // Really really hacky way to inject subtitles as file blobs into the video element
-    axios
-      .get(subtitle, { responseType: 'blob' })
-      .then(resp => {
-        const track = document.querySelector('track')
-        track?.setAttribute('src', URL.createObjectURL(resp.data))
-      })
-      .catch(() => {
-        console.log('Could not load subtitle.')
-      })
-
-    if (isFlv) {
-      const loadFlv = () => {
-        // Really hacky way to get the exposed video element from Plyr
-        const video = document.getElementById('plyr')
-        const flv = mpegts.createPlayer({ url: videoUrl, type: 'flv' })
-        flv.attachMediaElement(video)
-        flv.load()
-      }
-      loadFlv()
-    }
-  }, [videoUrl, isFlv, mpegts, subtitle])
-
-  // Common plyr configs, including the video source and plyr options
-  const plyrSource = {
-    type: 'video',
-    title: videoName,
-    poster: thumbnail,
-    tracks: [{ kind: 'captions', label: videoName, src: '', default: true }],
-  }
-  const plyrOptions: Plyr.Options = {
-    ratio: `${width ?? 16}:${height ?? 9}`,
-    fullscreen: { iosNative: true },
-  }
-  if (!isFlv) {
-    // If the video is not in flv format, we can use the native plyr and add sources directly with the video URL
-    plyrSource['sources'] = [{ src: videoUrl }]
-  }
-  return <Plyr id="plyr" source={plyrSource as Plyr.SourceInfo} options={plyrOptions} />
-}
 
 const VideoPreview: FC<{ file: OdFileObject }> = ({ file }) => {
   const { asPath } = useRouter()
@@ -81,48 +25,67 @@ const VideoPreview: FC<{ file: OdFileObject }> = ({ file }) => {
 
   const [menuOpen, setMenuOpen] = useState(false)
   const { t } = useTranslation()
-
-  // OneDrive generates thumbnails for its video files, we pick the thumbnail with the highest resolution
-  const thumbnail = `/api/thumbnail/?path=${asPath}&size=large${hashedToken ? `&odpt=${hashedToken}` : ''}`
-
-  // We assume subtitle files are beside the video with the same name, only webvtt '.vtt' files are supported
-  const vtt = `${asPath.substring(0, asPath.lastIndexOf('.'))}.vtt`
-  const subtitle = `/api/raw/?path=${vtt}${hashedToken ? `&odpt=${hashedToken}` : ''}`
-
-  // We also format the raw video file for the in-browser player as well as all other players
   const videoUrl = `/api/raw/?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`
-
-  const isFlv = getExtension(file.name) === 'flv'
-  const {
-    loading,
-    error,
-    result: mpegts,
-  } = useAsync(async () => {
-    if (isFlv) {
-      return (await import('mpegts.js')).default
-    }
-  }, [isFlv])
 
   return (
     <>
       <CustomEmbedLinkMenu path={asPath} menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <PreviewContainer>
-        {error ? (
-          <FourOhFour errorMsg={error.message} />
-        ) : loading && isFlv ? (
-          <Loading loadingText={t('Loading FLV extension...')} />
-        ) : (
-          <VideoPlayer
-            videoName={file.name}
-            videoUrl={videoUrl}
-            width={file.video?.width}
-            height={file.video?.height}
-            thumbnail={thumbnail}
-            subtitle={subtitle}
-            isFlv={isFlv}
-            mpegts={mpegts}
-          />
-        )}
+        <div className="items-center px-5 py-4 md:flex md:space-x-8">
+          <div className="rounded-lg border border-gray-900/10 px-8 py-20 text-center dark:border-gray-500/30">
+            <FontAwesomeIcon icon={getFileIcon(file.name, { video: Boolean(file.video) })} />
+            <div className="mt-6 text-sm font-medium line-clamp-3 md:w-28">{file.name}</div>
+          </div>
+
+          <div className="flex flex-col space-y-2 py-4 md:flex-1">
+            <div>
+              <div className="py-2 text-xs font-medium uppercase opacity-80">{t('Last modified')}</div>
+              <div>{formatModifiedDateTime(file.lastModifiedDateTime)}</div>
+            </div>
+
+            <div>
+              <div className="py-2 text-xs font-medium uppercase opacity-80">{t('File size')}</div>
+              <div>{humanFileSize(file.size)}</div>
+            </div>
+
+            <div>
+              <div className="py-2 text-xs font-medium uppercase opacity-80">{t('MIME type')}</div>
+              <div>{file.file?.mimeType ?? t('Unavailable')}</div>
+            </div>
+
+            <div>
+              <div className="py-2 text-xs font-medium uppercase opacity-80">{t('Hashes')}</div>
+              <table className="block w-full overflow-scroll whitespace-nowrap text-sm md:table">
+                <tbody>
+                  <tr className="border-y bg-white dark:border-gray-700 dark:bg-gray-900">
+                    <td className="bg-gray-50 py-1 px-3 text-left text-xs font-medium uppercase tracking-wider text-gray-700 dark:bg-gray-800 dark:text-gray-400">
+                      Quick XOR
+                    </td>
+                    <td className="whitespace-nowrap py-1 px-3 font-mono text-gray-500 dark:text-gray-400">
+                      {file.file.hashes?.quickXorHash ?? t('Unavailable')}
+                    </td>
+                  </tr>
+                  <tr className="border-y bg-white dark:border-gray-700 dark:bg-gray-900">
+                    <td className="bg-gray-50 py-1 px-3 text-left text-xs font-medium uppercase tracking-wider text-gray-700 dark:bg-gray-800 dark:text-gray-400">
+                      SHA1
+                    </td>
+                    <td className="whitespace-nowrap py-1 px-3 font-mono text-gray-500 dark:text-gray-400">
+                      {file.file.hashes?.sha1Hash ?? t('Unavailable')}
+                    </td>
+                  </tr>
+                  <tr className="border-y bg-white dark:border-gray-700 dark:bg-gray-900">
+                    <td className="bg-gray-50 py-1 px-3 text-left text-xs font-medium uppercase tracking-wider text-gray-700 dark:bg-gray-800 dark:text-gray-400">
+                      SHA256
+                    </td>
+                    <td className="whitespace-nowrap py-1 px-3 font-mono text-gray-500 dark:text-gray-400">
+                      {file.file.hashes?.sha256Hash ?? t('Unavailable')}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </PreviewContainer>
 
       <DownloadBtnContainer>
@@ -135,7 +98,11 @@ const VideoPreview: FC<{ file: OdFileObject }> = ({ file }) => {
           />
           <DownloadButton
             onClickCallback={() =>
-              window.open(`https://cors.isteed.cc/${getBaseUrl()}/api/raw/?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`)
+              window.open(
+                `https://cors.isteed.cc/${getBaseUrl()}/api/raw/?path=${asPath}${
+                  hashedToken ? `&odpt=${hashedToken}` : ''
+                }`
+              )
             }
             btnColor="teal"
             btnText={t('Proxy download')}
